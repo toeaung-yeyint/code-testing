c590020ba10837a899cb66f0593eadfa63cd8957
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-06-14 03:26:13 +1000

    removed dist folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/my-portfolio-draft/",
+	publicPath:
+		process.env.NODE_ENV === "production" ? "/my-portfolio-draft/" : "/",
 	plugins: [vue()],
 });
