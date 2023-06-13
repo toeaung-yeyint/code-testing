7c2848f1fee75bca82f1210ae91c28dfaf17f6b5
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-06-14 04:34:08 +1000

    removed dist folder

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Projects from "../views/Projects.vue";
 import ProjectDetails from "../views/ProjectDetails.vue";
@@ -25,7 +25,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHashHistory(),
+	history: createWebHistory("/my-portfolio-draft/"),
 	routes,
 });
 
