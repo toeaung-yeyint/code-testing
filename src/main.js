5ae9ef4a818656b7ae5cdd7e5ec0d66b98da1337
Toe Aung Yint <ToeAung.Yint@monash.edu>
2023-06-13 16:16:20 +1000

    removed dist

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import './style.css'
 import App from './App.vue'
 import router from './router/router.js'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+
+app.config.globalProperties.$routerBase = '/my-portfolio-draft/';
