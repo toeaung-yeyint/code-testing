1508455206b31f026cc502d62a7c3216f056e4ed
Toe Aung Yint <ToeAung.Yint@monash.edu>
2023-07-17 12:26:00 +1000

    just testing code

--- src/views/ProjectDetails.vue
+++ src/views/ProjectDetails.vue
@@ -2,25 +2,21 @@
   <h1>This is project {{ project.title }}!</h1>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      projects: [
-        { id: 1, title: "team-titles" },
-        { id: 2, title: "raise-or-drop" },
-        { id: 3, title: "image-object-detection" },
-      ],
-      project: null,
-    };
-  },
-  props: ["id"],
-  beforeMount() {
-    this.project = this.projects.find(
-      (project) => project.id === Number.parseInt(this.id)
-    );
-  },
-};
+<script setup>
+import { ref, onMounted } from "vue";
+const project = ref("");
+const projects = ref([
+  { id: 1, title: "team-titles" },
+  { id: 2, title: "raise-or-drop" },
+  { id: 3, title: "image-object-detection" },
+]);
+const props = defineProps(["id"]);
+console.log(props.id);
+onMounted(() => {
+  project.value = projects.value.find(
+    (project) => project.id === Number.parseInt(props.id)
+  );
+});
 </script>
 
 <style>
