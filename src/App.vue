41970a3c4dfd3f57e6a95e722a32cbd6ae7c2b85
Toe Aung Yint <toeaung.yint@monash.edu>
2023-07-17 11:56:38 +1000

    just testing code

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
-	<RouterLink to="/">Home</RouterLink> |
-	<RouterLink to="/projects">Projects</RouterLink> |
-	<RouterLink to="/about">About</RouterLink>
-	<RouterView />
+  <br />
+  <RouterLink to="/">Home</RouterLink> |
+  <RouterLink to="/projects">Projects</RouterLink> |
+  <RouterLink to="/about">About</RouterLink>
+  <RouterView />
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+a.router-link-exact-active {
+  color: white;
+  background: black;
+  padding: 10px;
+  border-radius: 5px;
+  text-decoration: none;
+}
+</style>
