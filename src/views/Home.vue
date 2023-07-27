f30944ebeefa3792341f352785e00403db1a5e3b
Toe Aung Yint <ToeAung.Yint@monash.edu>
2023-07-27 17:22:36 +1000

    code testing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,53 @@
 <template>
-	<h1>This is {{ $t("hello") }} page.</h1>
-	<LanguageSwitcher />
+  <div class="max-w-screen-lg mx-auto my-16 px-4">
+    <div class="grid grid-cols-3 gap-x-10">
+      <a href="https://www.google.com/" class="flex flex-col shadow-2xl group">
+        <div class="overflow-hidden">
+          <img
+            src="https://www.monash.edu/__data/assets/image/0011/2895167/hero.jpg"
+            alt=""
+            class="group-hover:scale-110 transition-scale duration-500 object-cover"
+          />
+        </div>
+        <h3
+          class="dark:text-gray-300 text-center relative p-3 text-sky-600 text-lg font-bold after:content-[''] after:w-full after:h-1 after:bg-gradient-to-r after:from-transparent after:via-[#007aff] after:to-transparent after:absolute after:top-0 after:left-0 after:opacity-0 group-hover:after:opacity-100 after:transition-opacity after:duration-500"
+        >
+          Testing
+        </h3>
+      </a>
+      <a href="https://www.google.com/" class="flex flex-col shadow-2xl group">
+        <div class="overflow-hidden">
+          <img
+            src="https://www.monash.edu/__data/assets/image/0011/2895167/hero.jpg"
+            alt=""
+            class="group-hover:scale-110 transition-scale duration-500 object-cover"
+          />
+        </div>
+        <h3
+          class="dark:text-gray-300 text-center relative p-3 text-sky-600 text-lg font-bold after:content-[''] after:w-full after:h-1 after:bg-gradient-to-r after:from-transparent after:via-[#007aff] after:to-transparent after:absolute after:top-0 after:left-0 after:opacity-0 group-hover:after:opacity-100 after:transition-opacity after:duration-500"
+        >
+          Testing
+        </h3>
+      </a>
+      <a href="https://www.google.com/" class="flex flex-col shadow-2xl group">
+        <div class="overflow-hidden">
+          <img
+            src="https://www.monash.edu/__data/assets/image/0011/2895167/hero.jpg"
+            alt=""
+            class="group-hover:scale-110 transition-scale duration-500 object-cover"
+          />
+        </div>
+        <h3
+          class="dark:text-gray-300 text-center relative p-3 text-sky-600 text-lg font-bold after:content-[''] after:w-full after:h-1 after:bg-gradient-to-r after:from-transparent after:via-[#007aff] after:to-transparent after:absolute after:top-0 after:left-0 after:opacity-0 group-hover:after:opacity-100 after:transition-opacity after:duration-500"
+        >
+          Testing
+        </h3>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import LanguageSwitcher from "../components/LanguageSwitcher.vue";
 </script>
 
 <style></style>
